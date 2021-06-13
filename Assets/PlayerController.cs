874d650d9ff3bbb2fdbc4b3014dcb1fbf6bd4807
iambalaam <guy.balaam@gmail.com>
2021-06-13 14:06:07 +0100

    Rotating girl

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -8,7 +8,9 @@ public class PlayerController : MonoBehaviour
     private float DEAD_ZONE = 0.3f;
     private float MOVE_SPEED = 8;
     private float MOVE_ACCN = 0.05f;
-
+    private float TURN_ACCN = 0.08f;
+    private float _currentRoatation = 100;
+    private float targetRotation = 100;
 
     private Vector2 _input;
     private Vector3 _currentMovement;
@@ -21,14 +23,20 @@ public class PlayerController : MonoBehaviour
     }
 
     void Update()
-    {        
-        Vector3 targetMovement = new Vector3(_input.x, 0, 0) * MOVE_SPEED * Time.deltaTime;
+    {
+        if (_input.x > 0) targetRotation = 100;
+        if (_input.x < 0) targetRotation = 260;
 
+
+        Vector3 targetMovement = new Vector3(_input.x, 0, 0) * MOVE_SPEED * Time.deltaTime;
+        float rotation = Mathf.Lerp(_currentRoatation, targetRotation, TURN_ACCN);
         Vector3 movement = Vector3.Lerp(_currentMovement, targetMovement, MOVE_ACCN);
 
         _controller.Move(movement);
+        transform.rotation = Quaternion.Euler(0, rotation, 0);
         _animator.SetFloat("moveSpeed", movement.magnitude);
         _currentMovement = movement;
+        _currentRoatation = rotation;
     }
 
     public void OnLStick(InputAction.CallbackContext ctx)
